8eaa298030e760c3281844d6d3aad9163b20d365
Vinay Sagar <bt22cse041@iiitn.ac.in>
2024-12-18 18:45:06 +0530

    Time: 2 ms (39.13%), Space: 15.2 MB (30.23%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -26,7 +26,21 @@ public:
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
-        vector<vector<int>> dp(n+1, vector<int> (2, -1));
-        return func(0,n,1,prices,dp);
+        // vector<vector<int>> dp(n+1, vector<int> (2, -1));
+        vector<int> front2(2, 0);
+        vector<int> front1(2, 0);
+        vector<int> curr(2, 0);
+        
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            curr[1] = max(-prices[ind]+front1[0], front1[1]);
+            curr[0] = max(prices[ind] + front2[1], front1[0]);
+            
+            front2 = front1;
+            front1 = curr;
+        }
+        
+        return curr[1];
+        // return func(0,n,1,prices,dp);
     }
 };
