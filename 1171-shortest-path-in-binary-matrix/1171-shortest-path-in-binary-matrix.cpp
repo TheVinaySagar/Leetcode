988778ff6f37bb873425ed673739138b0d81bddd
Vinay Sagar <bt22cse041@iiitn.ac.in>
2025-04-12 15:03:07 +0530

    1171-SHORTEST-PATH-IN-BINARY-MATRIX Stats: Time: 50 ms (25.78%), Space: 23.7 MB (79.11%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -6,21 +6,22 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         
-        set<pair<int,pair<int,int>>> st;
+        // set<pair<int,pair<int,int>>> st;
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<>> st;
         vector<vector<int>> dist(n, vector<int> (n,1e9));
         if(grid[0][0] != 0)
             return -1;
 
-        st.insert({1, {0,0}});
+        st.push({1, {0,0}});
         dist[0][0] = 1;
 
         while(!st.empty())
         {
-            auto to = st.begin();
-            int distance = (*to).first;
-            int x = (*to).second.first;
-            int y = (*to).second.second;
-            st.erase(to);
+            auto to = st.top();
+            int distance = to.first;
+            int x = to.second.first;
+            int y = to.second.second;
+            st.pop();
             
             for(int i = 0; i<8 ;i++)
             {
@@ -31,9 +32,9 @@ public:
                 {
                     if(distance + 1 < dist[nx][ny])
                     {
-                        st.erase({dist[nx][ny], {nx, ny}});
+                        // st.erase({dist[nx][ny], {nx, ny}});
                         dist[nx][ny] = distance + 1;
-                        st.insert({dist[nx][ny],{nx,ny}});
+                        st.push({dist[nx][ny],{nx,ny}});
                     }
                         
                 }
