7736cdb144b5ceb09726e1da6211fc1f6b60902e
Vinay Sagar <bt22cse041@iiitn.ac.in>
2025-05-18 12:11:46 +0530

    0424-LONGEST-REPEATING-CHARACTER-REPLACEMENT Stats: Time: 1 ms (81.02%), Space: 10.9 MB (42.96%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    bool func(vector<int> a, int k)
-    {
-        sort(a.begin(), a.end(), greater<>());
-        int sum = 0;
-        for(int i = 1; i < a.size(); i++)
-        {
-            if(a[i]==0) break;
-            sum += a[i];
-        }
-        return sum <= k; 
-    }
     int characterReplacement(string s, int k) {
         int n = s.size();
         int l = 0;
-        int maxi = -1;
-        vector<int> alpha(26,0);
-        for(int r=0;r<n;r++)
+        int maxi = 0;
+        vector<int> cha(26,0);
+        int maxifre = 0;
+        for(int r = 0; r < n; r++)
         {
-            alpha[s[r]-'A']++;
+            cha[s[r] - 'A']++;
+            maxifre = max(maxifre, cha[s[r]-'A']);
 
-            while(!func(alpha, k))
+            while((r - l + 1) - maxifre > k)
             {
-                alpha[s[l] - 'A']--;
+                cha[s[l] - 'A'] --;
+                // maxifre = max(maxifre, cha[s[l]-'A']);
                 l++;
             }
-
             maxi = max(maxi, r - l + 1);
         }
 
